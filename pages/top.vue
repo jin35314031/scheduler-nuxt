7670db0a234ab9c6de3b27a31caf184a8c6737b2
jin35314031 <jin35314031@gmail.com>
2020-08-11 16:13:40 +0900

    calendarDisplayFix

---
+++ pages/top.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <el-calendar>
+      <div
+        slot="dateCell"
+        slot-scope="{ date,data }">
+          <p :class="data.isSelected ? 'is-selected' : ''">
+           <!--  {{ data.day.split('-').slice(2).join('-') }}{{ data.isSelected ? '✔️' : ''}} -->
+          </p>
+
+       </div>
+    </el-calendar>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      calendarEvents:[
+      {
+        title:'報告会',
+        start:'2020-08-24',
+        end:'2020-08-24',
+      },
+     ]
+   }
+ }
+}
+</script>
+
+<style>
+  .is-selected {
+      color: #1989FA;
+    }
+</style>
