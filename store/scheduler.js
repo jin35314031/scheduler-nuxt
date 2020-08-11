05dfebcaaa44476515918c38a662c63f7e304513
jin35314031 <jin35314031@gmail.com>
2020-08-12 00:18:25 +0900

    createActionUnfinished

---
+++ store/scheduler.js
@@ -0,0 +1,26 @@
+import Vuex from 'vuex'
+
+export const state = () => ({
+  events: [{
+    title:'報告会',
+    start:'2020-08-24',
+    end:'2020-08-28',
+  },
+  ]
+})
+
+export const mutations = {
+  updateEvents:function(state,data){
+    console.log('mutation')
+    state.events = data
+  }
+}
+
+export const actions = {
+
+  createEventsAction(context,createEvent){
+    console.log(createEvent)
+    context.commit('updateEvents',createEvent)
+    console.log('action')
+  }
+}
