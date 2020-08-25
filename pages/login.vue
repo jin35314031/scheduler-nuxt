0c7bf77a5e144e07f515836846ded44ec19c2597
jin35314031 <jin35314031@gmail.com>
2020-08-25 16:39:01 +0900

    ログイン画面を修正。

--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,21 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
-    <el-form-item label="loginID">
-      <el-input v-model="form.loginId"></el-input>
-    </el-form-item>
-    <el-form-item label="password">
-      <el-input v-model="form.password" type="password"></el-input>
-    </el-form-item>
-    <el-button type="primary" v-on:click="login()">Login</el-button>
-  </el-form>
-
+  <div class="background">
+  <el-card class="box-card login">
+    <div>
+      <span style="margin-bottom:20px">Login</span>
+    </div>
+    <hr>
+    <el-form ref="form" :model="form" label-width="120px" style="margin-top:20px">
+      <el-form-item label="loginID">
+        <el-input v-model="form.loginId"></el-input>
+      </el-form-item>
+      <el-form-item label="password">
+        <el-input v-model="form.password" type="password"></el-input>
+      </el-form-item>
+      <el-button class="button" type="primary" v-on:click="login()">Login</el-button>
+    </el-form>
+  </el-card>
+  </div>
 </template>
 
 <script>
@@ -40,5 +47,27 @@
 </script>
 
 <style>
+  .box-card {
+    width: 480px;
+  }
+
+  .login {
+    position: relative;
+    top: 100px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    margin: auto;
+  }
+
+  .button{
+    float:right;
+    margin-bottom: 20px;
+  }
+  .background{
+    background-image:url("https://cdn.pixabay.com/photo/2019/01/02/19/29/background-3909536_1280.jpg");
+    height:1000px;
+    background-size:cover ;
+  }
 
 </style>
